9bd5c956b45f6cacf48e9cd69cf3871f190d0b46
McToluene <xclusiveniggaz10@live.com>
2021-08-28 23:04:50 +0100

    Dependency injection with spring

--- src/main/java/com/mctoluene/sfgdi/SfgDiApplication.java
+++ src/main/java/com/mctoluene/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package com.mctoluene.sfgdi;
 
+import com.mctoluene.sfgdi.controllers.ConstructorInjectedController;
 import com.mctoluene.sfgdi.controllers.MyController;
+import com.mctoluene.sfgdi.controllers.PropertyInjectedController;
+import com.mctoluene.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +16,18 @@ public class SfgDiApplication {
 		MyController myController = (MyController) context.getBean("myController");
 		String greeting = myController.sayHello();
 		System.out.println(greeting);
+
+		System.out.println("Property......");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) context.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("Setter......");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) context.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("Constructor......");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) context.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
 	}
 
 }
--- src/main/java/com/mctoluene/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/com/mctoluene/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package com.mctoluene.sfgdi.controllers;
 
 import com.mctoluene.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
